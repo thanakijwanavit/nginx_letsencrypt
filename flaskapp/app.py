ece213775d04da41b223ae93efb92f052d3f7fa2
Ubuntu <ubuntu@ip-172-31-31-97.us-west-2.compute.internal>
2019-07-02 01:37:03 +0000

    add jupyterlabfor arm

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -5,6 +5,39 @@ from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+html_return='''
+    <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">{}</h5>
+    <p class="card-text">{}</p>
+    <form action="/reset" method="post">
+        <button type="submit" class="btn btn-primary" name="reset_button" value="reset"> reset</button>
+    </form>
+  </div>
+  </body>
+</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    '''
 
 
 def get_hit_count():
@@ -18,8 +51,20 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+def reset_count():
+    cache.set('hits',0)
 
 @app.route('/')
 def hello():
+   count = get_hit_count()
+   return html_return.format('test',f'I have been seen {count} times')
+
+@app.route('/reset',methods=['GET', 'POST'])
+def reset():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    reset_count()
+    return 'the count has been reset from {}'.format(count)
+
+@app.route('/piak', methods=['GET', 'POST'])
+def piak():
+    return html_return.format('I am piak', '<h>I am a dickhead</h>')
--- nginx/restart.sh
+++ nginx/restart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+cp -r ../config ./config
 docker container stop nginxdo
 docker container rm nginxdo
 docker image rm nginxdo
 docker build -t nginxdo .
 docker run --name nginxdo --network host -d nginxdo
+sleep 5s
+docker container logs nginxdo
